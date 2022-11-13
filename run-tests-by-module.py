7dfab74e98814285ec73ec2574a2fffd71ed74b9
Loïc Estève <loic.esteve@ymail.com>
2022-11-13 16:24:39 +0100

    Fix to handle tests folders in scipy without __init__

--- run-tests-by-module.py
+++ run-tests-by-module.py
@@ -8,84 +8,84 @@ import itertools
 import unittest
 
 # This is the output of the command run from the scipy root folder:
-# find scipy -name tests | sort | perl -pe 's@\./@@g' | perl -pe 's@/@.@g'
+# find scipy -name tests | sort | perl -pe 's@/tests@@' | perl -pe 's@/@.@g'
 test_submodules_str = """
-scipy._build_utils.tests
-scipy.cluster.tests
-scipy.constants.tests
-scipy.fftpack.tests
-scipy.fft._pocketfft.tests
-scipy.fft.tests
-scipy.integrate._ivp.tests
-scipy.integrate.tests
-scipy.interpolate.tests
-scipy.io.arff.tests
-scipy.io._harwell_boeing.tests
-scipy.io.matlab.tests
-scipy.io.tests
-scipy._lib.tests
-scipy.linalg.tests
-scipy.misc.tests
-scipy.ndimage.tests
-scipy.odr.tests
-scipy.optimize.tests
-scipy.optimize._trustregion_constr.tests
-scipy.signal.tests
-scipy.sparse.csgraph.tests
-scipy.sparse.linalg._dsolve.tests
-scipy.sparse.linalg._eigen.arpack.tests
-scipy.sparse.linalg._eigen.lobpcg.tests
-scipy.sparse.linalg._eigen.tests
-scipy.sparse.linalg._isolve.tests
-scipy.sparse.linalg.tests
-scipy.sparse.tests
-scipy.spatial.tests
-scipy.spatial.transform.tests
-scipy.special.tests
-scipy.stats.tests
+scipy._build_utils
+scipy.cluster
+scipy.constants
+scipy.fftpack
+scipy.fft._pocketfft
+scipy.fft
+scipy.integrate._ivp
+scipy.integrate
+scipy.interpolate
+scipy.io.arff
+scipy.io._harwell_boeing
+scipy.io.matlab
+scipy.io
+scipy._lib
+scipy.linalg
+scipy.misc
+scipy.ndimage
+scipy.odr
+scipy.optimize
+scipy.optimize._trustregion_constr
+scipy.signal
+scipy.sparse.csgraph
+scipy.sparse.linalg._dsolve
+scipy.sparse.linalg._eigen.arpack
+scipy.sparse.linalg._eigen.lobpcg
+scipy.sparse.linalg._eigen
+scipy.sparse.linalg._isolve
+scipy.sparse.linalg
+scipy.sparse
+scipy.spatial
+scipy.spatial.transform
+scipy.special
+scipy.stats
 """
 
 test_submodules = test_submodules_str.split()
 
 expected_test_results_by_category = {
     "failed": [
-        "scipy.interpolate.tests",
-        "scipy.ndimage.tests",
-        "scipy.sparse.linalg._dsolve.tests",
-        "scipy.sparse.linalg._eigen.arpack.tests",
-        "scipy.special.tests",
+        "scipy.interpolate",
+        "scipy.ndimage",
+        "scipy.sparse.linalg._dsolve",
+        "scipy.sparse.linalg._eigen.arpack",
+        "scipy.special",
     ],
     "fatal error or timeout": [
-        "scipy.fft.tests",
-        "scipy.integrate._ivp.tests",
-        "scipy.integrate.tests",
-        "scipy.io.tests",
-        "scipy._lib.tests",
-        "scipy.linalg.tests",
-        "scipy.optimize.tests",
-        "scipy.signal.tests",
-        "scipy.sparse.linalg._isolve.tests",
-        "scipy.sparse.linalg.tests",
-        "scipy.sparse.tests",
-        "scipy.spatial.tests",
-        "scipy.stats.tests",
+        "scipy.fft",
+        "scipy.integrate._ivp",
+        "scipy.integrate",
+        "scipy.io",
+        "scipy._lib",
+        "scipy.linalg",
+        "scipy.optimize",
+        "scipy.signal",
+        "scipy.sparse.linalg._isolve",
+        "scipy.sparse.linalg",
+        "scipy.sparse",
+        "scipy.spatial",
+        "scipy.stats",
     ],
     "passed": [
-        "scipy._build_utils.tests",
-        "scipy.cluster.tests",
-        "scipy.constants.tests",
-        "scipy.fftpack.tests",
-        "scipy.fft._pocketfft.tests",
-        "scipy.io.arff.tests",
-        "scipy.io._harwell_boeing.tests",
-        "scipy.io.matlab.tests",
-        "scipy.misc.tests",
-        "scipy.odr.tests",
-        "scipy.optimize._trustregion_constr.tests",
-        "scipy.sparse.csgraph.tests",
-        "scipy.sparse.linalg._eigen.lobpcg.tests",
-        "scipy.sparse.linalg._eigen.tests",
-        "scipy.spatial.transform.tests",
+        "scipy._build_utils",
+        "scipy.cluster",
+        "scipy.constants",
+        "scipy.fftpack",
+        "scipy.fft._pocketfft",
+        "scipy.io.arff",
+        "scipy.io._harwell_boeing",
+        "scipy.io.matlab",
+        "scipy.misc",
+        "scipy.odr",
+        "scipy.optimize._trustregion_constr",
+        "scipy.sparse.csgraph",
+        "scipy.sparse.linalg._eigen.lobpcg",
+        "scipy.sparse.linalg._eigen",
+        "scipy.spatial.transform",
     ],
 }
 
