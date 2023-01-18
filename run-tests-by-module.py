42fe813a60780ae4187a2077bb0c2b0a1380a490
Loïc Estève <loic.esteve@ymail.com>
2023-01-18 13:43:40 +0100

    Sync with scikit-learn-tests-pyodide (#18)

--- run-tests-by-module.py
+++ run-tests-by-module.py
@@ -1,95 +1,48 @@
 import shlex
 import sys
 import itertools
-import unittest
 import asyncio
 
-
-# This is the output of the command run from the scipy root folder:
-# find scipy -name tests | sort | perl -pe 's@/@.@g'
-test_submodules_str = """
-scipy._build_utils.tests
-scipy.cluster.tests
-scipy.constants.tests
-scipy.fftpack.tests
-scipy.fft._pocketfft.tests
-scipy.fft.tests
-scipy.integrate._ivp.tests
-scipy.integrate.tests
-scipy.interpolate.tests
-scipy.io.arff.tests
-scipy.io._harwell_boeing.tests
-scipy.io.matlab.tests
-scipy.io.tests
-scipy._lib.tests
-scipy.linalg.tests
-scipy.misc.tests
-scipy.ndimage.tests
-scipy.odr.tests
-scipy.optimize.tests
-scipy.optimize._trustregion_constr.tests
-scipy.signal.tests
-scipy.sparse.csgraph.tests
-scipy.sparse.linalg._dsolve.tests
-scipy.sparse.linalg._eigen.arpack.tests
-scipy.sparse.linalg._eigen.lobpcg.tests
-scipy.sparse.linalg._eigen.tests
-scipy.sparse.linalg._isolve.tests
-scipy.sparse.linalg.tests
-scipy.sparse.tests
-scipy.spatial.tests
-scipy.spatial.transform.tests
-scipy.special.tests
-scipy.stats.tests
-"""
-
-test_submodules = test_submodules_str.split()
-
-expected_test_results_by_category = {
-    "failed": [
-        "scipy.fft.tests",
-        "scipy.interpolate.tests",
-        "scipy._lib.tests",
-        "scipy.ndimage.tests",
-        "scipy.signal.tests",
-        "scipy.sparse.linalg._dsolve.tests",
-        "scipy.sparse.linalg._eigen.arpack.tests",
-        "scipy.sparse.tests",
-        "scipy.spatial.tests",
-        "scipy.special.tests",
-    ],
-    "fatal error or timeout": [
-        "scipy.linalg.tests",
-        "scipy.optimize.tests",
-        "scipy.sparse.linalg._isolve.tests",
-        "scipy.sparse.linalg.tests",
-        "scipy.stats.tests",
-    ],
-    "passed": [
-        "scipy._build_utils.tests",
-        "scipy.cluster.tests",
-        "scipy.constants.tests",
-        "scipy.fftpack.tests",
-        "scipy.fft._pocketfft.tests",
-        "scipy.io.arff.tests",
-        "scipy.io._harwell_boeing.tests",
-        "scipy.io.matlab.tests",
-        "scipy.misc.tests",
-        "scipy.odr.tests",
-        "scipy.optimize._trustregion_constr.tests",
-        "scipy.sparse.csgraph.tests",
-        "scipy.sparse.linalg._eigen.lobpcg.tests",
-        "scipy.sparse.linalg._eigen.tests",
-        "scipy.spatial.transform.tests",
-    ],
-    "pytest usage error": [
-        "scipy.integrate._ivp.tests",
-    ],
-    "tests collection error": [
-        "scipy.integrate.tests",
-        "scipy.io.tests",
-    ],
+# Test submodules are from the output of the command run from the scikit-learn
+# root folder:
+# find sklearn -name tests | sort | perl -pe 's@/@.@g'
+expected_test_results = {
+    "scipy._build_utils.tests": ["passed"],
+    "scipy.cluster.tests": ["passed"],
+    "scipy.constants.tests": ["passed"],
+    "scipy.fftpack.tests": ["passed"],
+    "scipy.fft._pocketfft.tests": ["passed"],
+    "scipy.fft.tests": ["failed"],
+    "scipy.integrate._ivp.tests": ["pytest usage error"],
+    "scipy.integrate.tests": ["tests collection error"],
+    "scipy.interpolate.tests": ["failed"],
+    "scipy.io.arff.tests": ["passed"],
+    "scipy.io._harwell_boeing.tests": ["passed"],
+    "scipy.io.matlab.tests": ["passed"],
+    "scipy.io.tests": ["tests collection error"],
+    "scipy._lib.tests": ["failed"],
+    "scipy.linalg.tests": ["fatal error or timeout"],
+    "scipy.misc.tests": ["passed"],
+    "scipy.ndimage.tests": ["failed"],
+    "scipy.odr.tests": ["passed"],
+    "scipy.optimize.tests": ["fatal error or timeout"],
+    "scipy.optimize._trustregion_constr.tests": ["passed"],
+    "scipy.signal.tests": ["failed"],
+    "scipy.sparse.csgraph.tests": ["passed"],
+    "scipy.sparse.linalg._dsolve.tests": ["failed"],
+    "scipy.sparse.linalg._eigen.arpack.tests": ["failed"],
+    "scipy.sparse.linalg._eigen.lobpcg.tests": ["passed"],
+    "scipy.sparse.linalg._eigen.tests": ["passed"],
+    "scipy.sparse.linalg._isolve.tests": ["fatal error or timeout"],
+    "scipy.sparse.linalg.tests": ["fatal error or timeout"],
+    "scipy.sparse.tests": ["failed"],
+    "scipy.spatial.tests": ["failed"],
+    "scipy.spatial.transform.tests": ["passed"],
+    "scipy.special.tests": ["failed"],
+    "scipy.stats.tests": ["fatal error or timeout"],
 }
+
+test_submodules = expected_test_results.keys()
 
 
 async def _read_stream(stream, cb, timeout_without_output):
@@ -173,8 +126,8 @@ def run_tests_for_module(module_str):
     # some tests in scipy.interpolate e.g.
     # test_rbfinterp.py::TestRBFInterpolatorNeighborsInf::test_chunking can
     # take more than 60s to run
-    timeout_without_output = 120 if 'interpolate' in module_str else 60
-    command_str = f"node --experimental-fetch scipy-pytest.js -v {module_str}"
+    timeout_without_output = 120 if "interpolate" in module_str else 60
+    command_str = f"node --experimental-fetch scipy-pytest.js --pyargs {module_str} -v --durations 10"
     command_list = shlex.split(command_str)
     command_result = execute_command_with_timeout(
         command_list=command_list, timeout_without_output=timeout_without_output
@@ -219,7 +172,7 @@ def print_summary(module_results):
 
     print()
     print("-" * 80)
-    print("Grouped by category:")
+    print("Grouped by category")
     print("-" * 80)
 
     def fun(each):
@@ -236,17 +189,28 @@ def print_summary(module_results):
 
     sys.stdout.flush()
 
-    # Compare test results with expectations. Easiest way I found to compare
-    # dicts with a good error message is to use unittest
-    tc = unittest.TestCase()
-    # to show full info about the diff
-    tc.maxDiff = None
-    test_results_with_sets = {k: set(v) for k, v in test_results_by_category.items()}
-    expected_test_results_with_sets = {
-        k: set(v) for k, v in expected_test_results_by_category.items()
-    }
-    tc.assertDictEqual(expected_test_results_with_sets, test_results_with_sets)
+    mismatches = []
+    for each in module_results:
+        expected_categories = expected_test_results[each["module"]]
+        if each["category"] not in expected_categories:
+            message = (
+                f"{each['module']} result expected in {expected_categories}, "
+                f"got {each['category']!r} instead"
+            )
+            mismatches.append(message)
+
+    if mismatches:
+        mismatches_str = "\n".join(mismatches)
+        print()
+        print("-" * 80)
+        print("Unexpected test results")
+        print("-" * 80)
+        print(mismatches_str)
+
+        return 1
+
     print("Test results matched expected ones")
+    return 0
 
 
 def main():
@@ -258,6 +222,7 @@ def main():
         test_submodules = [custom_pytest_args]
 
     for module in test_submodules:
+        print()
         print("-" * 80, flush=True)
         print(module, flush=True)
         print("-" * 80, flush=True)
@@ -271,7 +236,8 @@ def main():
     # When using custom pytest args, we run a single pytest command and it does
     # not make sense to compare results to expectation
     if not custom_pytest_args:
-        print_summary(module_results)
+        exit_code = print_summary(module_results)
+        sys.exit(exit_code)
 
 
 if __name__ == "__main__":
