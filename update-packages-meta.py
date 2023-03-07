725e34629afa1b6c7b85fb7fc984e04d5f41e8c0
Loïc Estève <loic.esteve@ymail.com>
2023-03-07 09:08:21 +0100

    Fix version to follow PEP 440

--- update-packages-meta.py
+++ update-packages-meta.py
@@ -47,7 +47,7 @@ def update_scikit_learn():
 
     meta["source"]["url"] = url
     meta["source"]["sha256"] = sha256
-    meta["package"]["version"] = f"1.3.0.dev{commit_sha}"
+    meta["package"]["version"] = f"1.3.0.dev+{commit_sha}"
     meta["build"]["unvendor-tests"] = False
     yaml.dump(meta, meta_path.open("w"))
 
