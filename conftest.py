5bb2c94ef7699bf45a742081954c504d820b6b25
Loïc Estève <loic.esteve@ymail.com>
2023-04-05 06:10:33 +0200

    tweak

--- conftest.py
+++ conftest.py
@@ -1,85 +1,85 @@
 import re
-
 import pytest
 
-tests_to_xfail = [
+xfail = pytest.mark.xfail
+skip = pytest.mark.skip
+
+tests_to_mark = [
     # scipy/_lib/tests
-    "test__threadsafety.py::test_parallel_threads",
-    "test__util.py::test_pool",
-    "test__util.py::test_mapwrapper_parallel",
-    "test_ccallback.py::test_threadsafety",
-    "test_import_cycles.py::test_modules_importable",
+    (
+        "test__threadsafety.py::test_parallel_threads",
+        xfail,
+        "no threading support",
+    ),
+    ("test__threadsafety.py::test_parallel_threads", xfail, "no threading support"),
+    ("test__util.py::test_pool", xfail, "no process support"),
+    ("test__util.py::test_mapwrapper_parallel", xfail, "no process support"),
+    ("test_ccallback.py::test_threadsafety", xfail, "no threading support"),
+    ("test_import_cycles.py::test_modules_importable", xfail, "no process support"),
     # scipy/fft/tests
-    "test_fft_function.py::test_fft_function",
-    "test_multithreading.py::test_threaded_same",
-    "test_multithreading.py::test_mixed_threads_processes",
-    "test_numpy.py::TestFFTThreadSafe",
-    "test_numpy.py::test_multiprocess",
+    ("test_fft_function.py::test_fft_function", xfail, "no process support"),
+    ("test_multithreading.py::test_threaded_same", xfail, "no threading support"),
+    (
+        "test_multithreading.py::test_mixed_threads_processes",
+        xfail,
+        "no threading support",
+    ),
+    ("test_numpy.py::TestFFTThreadSafe", xfail, "no threading support"),
+    ("test_numpy.py::test_multiprocess", xfail, "no process support"),
+    # scipy/linalg tests
+    ("test_basic.py.+test_hermitian", skip, "memory corruption"),
+    ("test_basic.py.+TestLstsq.+random_exact", skip, "memory corruption"),
+    ("test_blas.+test_complex_dotu", skip, "signature mismatch"),
+    ("test_cython_blas.+complex", skip, "signature mismatch"),
+    ("test_decomp_cossin", skip, "missing symbol"),
+    ("test_lapack.py.+larfg_larf", skip, "missing symbol"),
+    ("test_lapack.py.+geqrt_gemqrt", skip, "missing symbol"),
+    ("test_lapack.py.+tpqrt_tpmqrt", skip, "missing symbol"),
+    ("test_lapack.py.+test_geqrfp", skip, "missing symbol"),
+    ("test_lapack.py.+orcsd_uncsd", skip, "missing symbol"),
+    ("test_lapack.py.+test_gtsvx_error_singular", skip, "missing symbol"),
     # scipy/ndimage/tests
-    "test_filters.py::TestThreading",
+    ("test_filters.py::TestThreading", xfail, "no threading support"),
+    # scipy/optimize/tests
+    ("test_minpack.py.+test_reentrant_func", skip, "memory corruption"),
     # scipy/signal/tests
-    "test_signaltools.py::TestMedFilt.test_medfilt2d_parallel",
-    # scipy/sparse/linalg/_dsolve/tests
-    "test_linsolve.py::TestSplu.test_threads_parallel",
-    # scipy/sparse/linalg/_eigen/arpack/tests
-    "test_arpack.py::test_parallel_threads",
-    # scipy/spatial/tests
-    "test_kdtree.py::test_query_ball_point_multithreading",
-    "test_kdtree.py::test_ckdtree_parallel",
+    (
+        "test_signaltools.py::TestMedFilt.test_medfilt2d_parallel",
+        xfail,
+        "no threading support",
+    ),
     # scipy/sparse/tests
-    "test_sparsetools.py::test_threads",
+    ("test_linsolve.py::TestSplu.test_threads_parallel", xfail, "no threading support"),
+    ("test_arpack.py::test_parallel_threads", xfail, "no threading support"),
+    ("test_propack", skip, "signature mismatch"),
+    ("test_gcrotmk.+test_cornercase", skip, "memory corruption"),
+    ("test_iterative.+precond_dummy", skip, "memory corruption"),
+    ("test_iterative.+test_convergence", skip, "memory corruption"),
+    ("test_iterative.+gcrotmk", skip, "memory corruption"),
+    ("test_iterative.+lgmres", skip, "memory corruption"),
+    ("test_iterative.+test_maxiter$", skip, "memory corruption"),
+    ("test_sparsetools.py::test_threads", xfail, "no threading support"),
+    # scipy/spatial/tests
+    (
+        "test_kdtree.py::test_query_ball_point_multithreading",
+        xfail,
+        "no threading support",
+    ),
+    ("test_kdtree.py::test_ckdtree_parallel", xfail, "no threading support"),
+    # scipy/stats/tests
+    (
+        "test_resampling.+TestMonteCarloHypothesisTest.+test_against_anderson.+logistic",
+        skip,
+        "memory corruption",
+    ),
 ]
 
-# Those tests need to be skipped because they crash Pyodide, typically
-# signature mismatch or memory corruption
-tests_to_skip = [
-    # scipy/linalg tests
-    # memory corruption
-    "test_basic.py.+test_hermitian",
-    # memory corruption
-    "test_basic.py.+TestLstsq.+random_exact",
-    # signature mismatch
-    "test_blas.+test_complex_dotu",
-    # signature mismatch
-    "test_cython_blas.+complex",
-    # missing symbol
-    "test_decomp_cossin",
-    # signature mismatch
-    "test_lapack.py.+larfg_larf",
-    # missing symbols
-    "test_lapack.py.+geqrt_gemqrt",
-    "test_lapack.py.+tpqrt_tpmqrt",
-    "test_lapack.py.+test_geqrfp",
-    "test_lapack.py.+orcsd_uncsd",
-    "test_lapack.py.+test_gtsvx_error_singular",
-    # scipy/optimize/tests
-    # memory corruption
-    "test_minpack.py.+test_reentrant_func",
-    # scipy/sparse/tests
-    # signature mismatch
-    "test_propack",
-    # memory corruption
-    "test_gcrotmk.+test_cornercase",
-    "test_iterative.+precond_dummy",
-    "test_iterative.+test_convergence",
-    "test_iterative.+gcrotmk",
-    "test_iterative.+lgmres",
-    "test_iterative.+test_maxiter$",
-    # scipy/stats/tests
-    # seems like a memory corruption (not deterministic not always the same
-    # parametetrized tests that fails)
-    "test_resampling.+TestMonteCarloHypothesisTest.+test_against_anderson.+logistic",
-]
 
 def pytest_collection_modifyitems(config, items):
-    xfail_marker = pytest.mark.xfail(reason=("Known Pyodide limitation"))
-    skip_marker = pytest.mark.skip(reason=("Avoid crashing Pyodide"))
     for item in items:
         path, line, name = item.reportinfo()
         path = str(path)
         full_name = f"{path}::{name}"
-        if any(re.search(each, full_name) for each in tests_to_xfail):
-            item.add_marker(xfail_marker)
-
-        if any(re.search(each, full_name) for each in tests_to_skip):
-            item.add_marker(skip_marker)
+        for pattern, mark, reason in tests_to_mark:
+            if re.search(pattern, full_name):
+                item.add_marker(mark(reason=reason))
